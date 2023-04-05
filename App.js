475fd8754a246c2244473f3d51ef81c7214da990
Shruthika <100517924+Shruthika16@users.noreply.github.com>
2023-04-05 12:45:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import MovieBooking from './Components/MovieBooking';
+
+function App() {
+  return (
+    <div className="App">
+     <MovieBooking/>
+    </div>
+  );
+}
+
+export default App;
